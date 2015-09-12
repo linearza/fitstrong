2056bb275254c9b51760057d121ab5139ee333e8
Michiel <linearza@users.noreply.github.com>
2015-09-12 19:05:27 +0200

    Demo of adding data, with relationships saved to both sides

--- app/controllers/weekly-schedule.js
+++ app/controllers/weekly-schedule.js
@@ -3,6 +3,27 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
   // setup attrs
-  schedules: []
+  schedules: [],
+
+  actions: {
+
+    add() {
+
+      let schedule = this.get('schedules.firstObject');
+
+      let slot = this.store.createRecord('slot', {
+        description: 'whatever',
+        schedule: schedule,
+        title: 'New slot'
+      });
+
+      slot.save().then(slot => {
+        schedule.get('slots').addObject(slot);
+        schedule.save();
+      });
+
+    }
+
+  }
 
 });
