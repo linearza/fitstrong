fd0aa600c6e36ac5e0aa6ee99b93939aeb21e3a8
Michiel <linearza@users.noreply.github.com>
2015-09-13 10:55:33 +0200

    Jshint fixes

--- app/pods/training-roster/component.js
+++ app/pods/training-roster/component.js
@@ -1,9 +1,5 @@
 import Ember from 'ember';
 
-const {
-  computed, alias
-} = Ember;
-
 export default Ember.Component.extend({
 
   tagName: 'table',
--- app/pods/weekly-schedule/route.js
+++ app/pods/weekly-schedule/route.js
@@ -1,9 +1,5 @@
 import Ember from 'ember';
 
-const {
-  RSVP
-} = Ember;
-
 export default Ember.Route.extend({
 
   model: function() {
